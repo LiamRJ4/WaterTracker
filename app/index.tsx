d3c052df786f7a119384fd46644cfc8f24912306
Liam Jones <liamrjones1211@gmail.com>
2025-08-02 21:53:31 +0100

    index.tsx now conatins basic main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const MAX_WATER = 8;
 
 export default function Index() {
+
+  const [glasses, setGlasses] = useState(0);
+
+  const addGlass = () => {
+    if (glasses < MAX_WATER) setGlasses(glasses + 1);
+  };
+
+  const removeGlass = () => {
+    if (glasses > 0) setGlasses(glasses - 1);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View 
+    style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>Water Tracker!</Text>
+      <Text style={styles.counter}>
+        {glasses} / {MAX_WATER} glasses
+      </Text>
+      <View style={styles.buttonGroup}>
+      <Button title="➕ Add" onPress={addGlass} />
+      <Button title="➖ Remove" onPress={removeGlass} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#e0f7fa', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    padding: 20, 
+  }, 
+  title: {
+    fontSize: 28, 
+    fontWeight: 'bold', 
+    marginBottom: 16, 
+    color: '#0077b6', 
+  }, 
+  counter: {
+    fontSize: 22, 
+    marginBottom: 24, 
+  }, 
+  buttonGroup: {
+    flexDirection: 'row', 
+    gap: 10, 
+  },
+});
