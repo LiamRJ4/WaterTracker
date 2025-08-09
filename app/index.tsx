e043fe9106fea57e3f3392b05d03187426e10442
Liam Jones <liamrjones1211@gmail.com>
2025-08-09 19:46:46 +0100

    Added animated progress bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Animated, Button, StyleSheet, Text, View } from "react-native";
 
 const MAX_WATER = 8;
 const STORAGE_KEY_COUNT = 'WATER_COUNT';
@@ -8,9 +8,15 @@ const STORAGE_KEY_DATE = 'LAST_DATE';
 
 export default function Index() {
 
+  // Set constants for water count and date
+
   const [glasses, setGlasses] = useState(0);
 
   const getToday = () => new Date().toISOString().split('T')[0];
+
+  const progressAnim = useRef(new Animated.Value(0)).current;
+
+  // Load stored data and reset logic for daily use
 
   useEffect(() => {
     const loadData = async () => {
@@ -29,10 +35,23 @@ export default function Index() {
     loadData();
   }, []);
 
+  // Store data when it changes
+
   useEffect(() => {
     AsyncStorage.setItem(STORAGE_KEY_COUNT, glasses.toString());
     AsyncStorage.setItem(STORAGE_KEY_DATE, getToday());
+
+    // Animate progress bar
+
+    Animated.timing(progressAnim, {
+      toValue: glasses / MAX_WATER, 
+      duration: 500, 
+      useNativeDriver: false,
+    }).start();
+
   }, [glasses]);
+
+  // Add, remove and reset logic
 
   const addGlass = () => {
     if (glasses < MAX_WATER) setGlasses(glasses + 1);
@@ -44,6 +63,11 @@ export default function Index() {
 
   const reset = () => setGlasses(0);
 
+  const progressWidth = progressAnim.interpolate({
+    inputRange: [0,1], 
+    outputRange: ['0%', '100%'],
+  });
+
   return (
     <View 
     style={styles.container}
@@ -52,6 +76,16 @@ export default function Index() {
       <Text style={styles.counter}>
         {glasses} / {MAX_WATER} glasses
       </Text>
+      <View style={styles.progressBarBackground}>
+        <Animated.View
+          style={[
+            styles.progressBarFill,
+            {width: progressWidth}, 
+            glasses >= MAX_WATER && { backgroundColor: '#4ade80'},
+          ]}
+        />
+
+      </View>
       <View style={styles.buttonGroup}>
       <Button title="➕ Add" onPress={addGlass} />
       <Button title="➖ Remove" onPress={removeGlass} />
@@ -82,5 +116,17 @@ const styles = StyleSheet.create({
   buttonGroup: {
     flexDirection: 'row', 
     gap: 10, 
+  },
+  progressBarBackground: {
+    height: 20,
+    width: '80%',
+    backgroundColor: '#bae6fd',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginBottom: 24,
+  },
+  progressBarFill: {
+    height: '100%', 
+    backgroundColor: '#38bdf8'
   },
 });
